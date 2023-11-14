4993a5a4215a422ea3295291aff402a38a7f00db
Athena <bqh1402003@gmail.com>
2023-11-14 20:08:06 +0700

    init commit: print request

---
+++ PrintRequest/printAttributes.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./globalPrintRequest.css" />
+    <link rel="stylesheet" href="./printAttributes.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" />
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+    <div class="print-attributes-container">
+        <div class="chn-thuc-tnh">Chọn thuộc tính in</div>
+        <div class="pages-to-print-parent">
+            <div class="pages-to-print">
+                <div class="group-container">
+                    <div class="rectangle-parent4">
+                        <div class="group-child4"></div>
+                        <div class="div">1 - 33</div>
+                    </div>
+                    <div class="ellipse-parent">
+                        <div class="ellipse-div"></div>
+                        <div class="pages">Pages</div>
+                    </div>
+                </div>
+                <div class="ellipse-container">
+                    <div class="group-child6"></div>
+                    <div class="all2">All</div>
+                </div>
+            </div>
+            <div class="pages-to-print1">Pages to Print</div>
+        </div>
+        <div class="duplex">
+            <div class="duplex-printing">Duplex Printing</div>
+            <div class="duplex-yes">
+                <button class="duplex-box"></button>
+                <div class="duplex-text">Yes</div>
+            </div>
+            <div class="group-div">
+                <button class="duplex-box"></button>
+                <div class="duplex-text">No</div>
+            </div>
+        </div>
+        <div class="orientation">
+            <div class="orientation-parent">
+                <div class="orientation1">Orientation</div>
+                <div class="group-parent">
+                    <div class="rectangle-parent1">
+                        <button class="duplex-box"></button>
+                        <div class="duplex-text">Landscape</div>
+                    </div>
+                    <div class="rectangle-parent2">
+                        <button class="duplex-box"></button>
+                        <div class="duplex-text">Portrait</div>
+                    </div>
+                    <div class="rectangle-parent3">
+                        <button class="duplex-box"></button>
+                        <div class="duplex-text">All</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="rectangle-parent">
+            <button class="duplex-box"></button>
+            <div class="duplex-text">Quay lại</div>
+        </div>
+        <div class="rectangle-group">
+            <button class="duplex-box"></button>
+            <div class="duplex-text">Xác nhận</div>
+        </div>
+        <div class="page-layout-parent">
+            <div class="page-layout">
+                <div class="a6">
+                    <b class="a61">A6</b>
+                </div>
+                <div class="a5">
+                    <b class="a51">A5</b>
+                </div>
+                <div class="a4">
+                    <b class="a41">A4</b>
+                </div>
+                <div class="a3">
+                    <b class="a31">A3</b>
+                </div>
+                <div class="a2">
+                    <b class="a21">A2</b>
+                </div>
+                <div class="a1">
+                    <b class="a11">A1</b>
+                </div>
+                <div class="a0">
+                    <b class="a01">A0</b>
+                </div>
+            </div>
+            <div class="page-layout1">Page Layout</div>
+        </div>
+
+
+    </div>
+</body>
+
+</html>
